ae2537dfe13b1a9437dcf9a41070d8e8083d28ef
Samuel <06088@farnborough.ac.uk>
2021-07-01 17:58:46 +0100

    xdirection and ydirection as opposed to just direction

---
+++ maze.js
@@ -0,0 +1,133 @@
+let size = 30;
+let xUserPos = 300;
+let yUserPos = 300;
+let xTilePos;
+let yTilePos;
+let sprite;
+let speed = 2;
+
+let xTilePosArr = [];
+let yTilePosArr = [];
+
+let tiles = [[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+             [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+function setup() {
+    createCanvas(tiles.length*size, tiles.length*size); //sets canvas size to array size
+}
+
+function preload() {
+    sprite = loadImage('http://127.0.0.1:8887/assets/sprite.png'); //preloads character image
+}
+  
+function draw() {
+    background(220); //sets canvas background colour
+
+    //Map creation:
+    for (let j = 0; j < tiles.length; j++) {
+        for (let i = 0; i < tiles[j].length; i++) {
+            xTilePos = j * size; //sets x position of tile
+            yTilePos = i * size; //sets y position of tile
+
+            if (tiles[i][j] == 0) { //determines whether a tile should be placed at position
+                continue;
+            } else {
+                stroke(180);
+                rect(xTilePos, yTilePos, size, size); //spawns tile
+                xTilePosArr[j * size + i] = xTilePos;
+                yTilePosArr[j * size + i] = yTilePos;
+            }
+        }
+    }
+
+    //Canvas collision:
+    if (xUserPos < 0) {
+        xUserPos += speed; //causes collision with left canvas wall
+    }
+
+    if (xUserPos > (tiles.length * size) - size) {
+        xUserPos -= speed; //causes collision with right canvas wall
+    }
+
+    if (yUserPos < 0) {
+        yUserPos += speed; //causes collision with top canvas wall
+    }
+
+    if (yUserPos > (tiles.length * size) - size) {
+        yUserPos -= speed; //causes collision with bottom canvas wall
+    }
+
+    //Wall collision:
+    for (i = 0; i < xTilePosArr.length; i++) {
+        if (xUserPos > xTilePosArr[i] - size && xUserPos < xTilePosArr[i] + size && yUserPos > yTilePosArr[i] - size && yUserPos < yTilePosArr[i] + size) {
+            if (xDirection == "left") {
+                xUserPos += speed; //forces opposite speed to users motion causing user to stop 
+            }
+            
+            if (xDirection == "right") {
+                xUserPos -= speed;
+            }
+            
+            if (yDirection == "up" && xDirection == "") {
+                yUserPos += speed;
+            }
+            
+            if (yDirection == "down" && xDirection == "") {
+                yUserPos -= speed;
+            }
+        }
+    }
+
+    image(sprite, xUserPos, yUserPos, size, size); //spawns sprite
+
+    //Character movement:
+    if (keyIsDown(65)) {
+        xUserPos -= speed; //moves character left when A is being held
+        xDirection = "left"; 
+    } else if (keyIsDown(68)) {
+        xUserPos += speed; //moves character right when D is being held
+        xDirection = "right";
+    } else {
+        xDirection = ""; 
+    }
+    
+    if (keyIsDown(87)) {
+        yUserPos -= speed; //moves character up when W is being held
+        yDirection = "up";
+    } else if (keyIsDown(83)) {
+        yUserPos += speed; //moves character down when S is being held
+        yDirection = "down";
+    } else {
+        yDirection = "";
+    }
+}
