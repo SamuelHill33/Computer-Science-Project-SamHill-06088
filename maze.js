1867280388c89b4d65c67478df8e87317dc7731e
SamuelHill33 <06088@farnborough.ac.uk>
2021-07-02 16:15:29 +0100

    dljdjd

--- maze.js
+++ maze.js
@@ -1,15 +1,11 @@
 let size = 30;
-let xUserPos = 300;
-let yUserPos = 300;
-let xTilePos;
-let yTilePos;
+let xUserPos = 300 - (size / 2);
+let yUserPos = 400 - (size / 2);
 let sprite;
 let speed = 2;
+let tilesArr = [];
 
-let xTilePosArr = [];
-let yTilePosArr = [];
-
-let tiles = [[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+let mazeMap = [[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
              [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,10 +15,10 @@ let tiles = [[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 
              [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -42,31 +38,48 @@ let tiles = [[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
+class Tile {
+    constructor(xTilePos, yTilePos, isWall) {
+        this.xTilePos = xTilePos;
+        this.yTilePos = yTilePos;
+        this.isWall = isWall;
+    }
+
+    getXTilePos() {
+        return this.xTilePos;
+    }
+
+    getYTilePos() {
+        return this.yTilePos;
+    }
+
+    getIsWall() {
+        return this.isWall;
+    }
+}
+
 function setup() {
-    createCanvas(tiles.length*size, tiles.length*size); //sets canvas size to array size
+    createCanvas(mazeMap.length*size, mazeMap.length*size); //sets canvas size to array size
 }
 
 function preload() {
-    sprite = loadImage('http://127.0.0.1:8887/assets/sprite.png'); //preloads character image
+    sprite = loadImage('http://127.0.0.1:5500/assets/sprite.png'); //preloads character image
 }
   
 function draw() {
-    background(220); //sets canvas background colour
-
-    //Map creation:
-    for (let j = 0; j < tiles.length; j++) {
-        for (let i = 0; i < tiles[j].length; i++) {
-            xTilePos = j * size; //sets x position of tile
-            yTilePos = i * size; //sets y position of tile
-
-            if (tiles[i][j] == 0) { //determines whether a tile should be placed at position
-                continue;
+    //Maze creation:
+    for (let j = 0; j < mazeMap.length; j++) {
+        for (let i = 0; i < mazeMap[j].length; i++) {
+            if (mazeMap[i][j] == 0) { //determines whether a tile should be placed at position
+                fill(150);
+                stroke(100);
+                tilesArr[j * size + i] = new Tile(i * size, j * size, false);
             } else {
-                stroke(180);
-                rect(xTilePos, yTilePos, size, size); //spawns tile
-                xTilePosArr[j * size + i] = xTilePos;
-                yTilePosArr[j * size + i] = yTilePos;
+                fill(20, 20, 100);
+                noStroke();
+                tilesArr[j * size + i] = new Tile(i * size, j * size, true);
             }
+            rect(tilesArr[j * size + i].getXTilePos(), tilesArr[j * size + i].getYTilePos(), size, size); //spawns tile
         }
     }
 
@@ -75,7 +88,7 @@ function draw() {
         xUserPos += speed; //causes collision with left canvas wall
     }
 
-    if (xUserPos > (tiles.length * size) - size) {
+    if (xUserPos > (mazeMap.length * size) - size) {
         xUserPos -= speed; //causes collision with right canvas wall
     }
 
@@ -83,55 +96,42 @@ function draw() {
         yUserPos += speed; //causes collision with top canvas wall
     }
 
-    if (yUserPos > (tiles.length * size) - size) {
+    if (yUserPos > (mazeMap.length * size) - size) {
         yUserPos -= speed; //causes collision with bottom canvas wall
     }
 
     //Wall collision:
-    for (i = 0; i < xTilePosArr.length; i++) {
-        if (xUserPos > xTilePosArr[i] - size && xUserPos < xTilePosArr[i] + size && yUserPos > yTilePosArr[i] - size && yUserPos < yTilePosArr[i] + size) {           
-            if (xDirection == "left") {
-                xUserPos += speed; //forces opposite speed to users motion causing user to stop
-            }
-            
-            else if (xDirection == "right") {
-                xUserPos -= speed;
-            }
-            
-            else if (yDirection == "up") {
-                yUserPos += speed;
-            }
-            
-            else if (yDirection == "down") {
-                yUserPos -= speed;
-            }
+    //xUserPos > tilesArr[i].getXTilePos() - size && xUserPos < tilesArr[i].getXTilePos() + size && yUserPos > tilesArr[i].getYTilePos() - size && yUserPos < tilesArr[i].getYTilePos() + size
+    
+    for (let i = 0; i < tilesArr.length; i++) {
+        if (xUserPos > tilesArr[i].getXTilePos() - size && xUserPos < tilesArr[i].getXTilePos() + size && yUserPos > tilesArr[i].getYTilePos() - size && yUserPos < tilesArr[i].getYTilePos() + size) {
+            console.log("contact");
         }
+        // if (keyIsPressed) {
+        //     console.log(xUserPos + " " + yUserPos + " " + tilesArr[i].getXTilePos() + " " + tilesArr[i].getYTilePos());
+        //     if (tilesArr[i].getIsWall() == true) {
+        //         console.log("wall has been hit... hopefully");
+        //         speed = speed * -1;
+        //     }
+        // }
     }
 
-    //Character movement:
+    //Character movement:s
+    image(sprite, xUserPos, yUserPos, size, size); //spawns sprite
+
     if (keyIsDown(65)) {
         xUserPos -= speed; //moves character left when A is being held
-        xDirection = "left"; 
-        yDirection = "none"; 
     }
     
     if (keyIsDown(68)) {
         xUserPos += speed; //moves character right when D is being held
-        xDirection = "right";
-        yDirection = "none"; 
     }
     
     if (keyIsDown(87)) {
         yUserPos -= speed; //moves character up when W is being held
-        yDirection = "up";
-        xDirection = "none"; 
     } 
     
     if (keyIsDown(83)) {
         yUserPos += speed; //moves character down when S is being held
-        yDirection = "down";
-        xDirection = "none"; 
     }
-
-    image(sprite, xUserPos, yUserPos, size, size); //spawns sprite
 }
