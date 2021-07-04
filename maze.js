528a6c611c3912cc16c6a3f9127a77e3063a8017
Samuel <06088@farnborough.ac.uk>
2021-07-04 18:05:55 +0100

    finished iteration 1

--- maze.js
+++ maze.js
@@ -3,189 +3,162 @@ let xUserPos = 300 - (size / 2);
 let yUserPos = 400 - (size / 2);
 let sprite;
 let speed = 2;
-let tilesArr = [];
 
-let mazeMap = [
-[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-[0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+let mazeMap = [ //2D array of tile positions in maze -- 0: path tile -- 1: wall tile -- 2: start tile -- 3: target tile -- 4: door tile -- 5: key tile -- 6: vent tile, etc...
+[0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+[0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+[0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+[0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0],
+[1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+[0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+[0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+[0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+[0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+[0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+[1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+[0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+[0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+[0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+[0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1],
+[1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+[1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1],
+[1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]]
 
-class GridRef {
+class GridRef { //a class that instantiates a coordinate pair object
     constructor(xTilePos, yTilePos) {
         this.xTilePos = xTilePos;
         this.yTilePos = yTilePos;
     }
 
     getXTilePos() {
-        return this.xTilePos;
+        return this.xTilePos; //returns x position of coordinate pair
     }
 
     getYTilePos() {
-        return this.yTilePos;
+        return this.yTilePos; //returns y position of coordinate pair
     }
 }
 
-class Grid {
-    tilesMap = new Map();
-    constructor(mazeMap) {
-        for (let j = 0; j < mazeMap.length; j++) {
-            for (let i = 0; i < mazeMap[j].length; i++) {
-                let gridRef = new GridRef(j * size, i * size);
-                this.tilesMap.set(gridRef, new Tile(gridRef, size, mazeMap[i][j] == 1));
-                
-            }
-        }
-    }
-
-    draw() {
-        for (let [gridRef, tile] of this.tilesMap) {
-            tile.draw();
-        }
-        // if (mazeMap[i][j] == 0) { //determines whether a tile should be placed at position
-        //     fill(150);
-        //     stroke(100);
-        //     tilesArr[j * size + i] = new Tile(i * size, j * size, false);
-        // } else {
-        //     fill(20, 20, 100);
-        //     noStroke();
-        //     tilesArr[j * size + i] = new Tile(i * size, j * size, true);
-        // }
-        // rect(tilesArr[j * size + i].getXTilePos(), tilesArr[j * size + i].getYTilePos(), size, size); //spawns tile
-    }
-}
-
-class Tile {
-    constructor(gridRef, size, isWall) {
+class Tile { //a class that instantiates a tile object
+    constructor(gridRef, size) { //tile contains grid reference and size properties
         this.gridRef = gridRef;
         this.size = size;
-        this.isWall = isWall;
     }
 
     getGridRef() {
-        return this.gridRef;
+        return this.gridRef; //returns the tiles grid reference object
     }
 
-    getIsWall() {
-        return this.isWall;
+    getSize() {
+        return this.size; //returns the tiles size (square side length value)
     }
-    draw() {
-        if (this.isWall) { //determines whether a tile should be placed at position
-            fill(20, 20, 100);
-            noStroke();
-        } else {
-            fill(150);
-            stroke(100);
-        }
+
+    draw() { //a method for displaying the tile object onto the canvas
+        fill(200); //colours tile
+        stroke(180); //outlines tile
         rect(this.gridRef.getXTilePos(), this.gridRef.getYTilePos(), this.size, this.size); //spawns tile
     }
 }
 
-function setup() {
-    createCanvas(mazeMap.length*size, mazeMap.length*size); //sets canvas size to array size
+class WallTile extends Tile { //a class that extends from a tile class and instantiates a wall tile object
+    constructor(gridRef, size) {
+        super(gridRef, size);
+    }
+
+    draw() { //wall tile has different draw method that will overwrite parent class draw method
+        fill(100, 70, 50); 
+        noStroke(); //removes outline (no outline)
+        rect(this.gridRef.getXTilePos(), this.gridRef.getYTilePos(), this.size, this.size); 
+    }
 }
 
-function preload() {
-    sprite = loadImage('http://127.0.0.1:5500/assets/sprite.png'); //preloads character image
-}
-  
-function draw() {
-    grid = new Grid(mazeMap);
-    grid.draw();
-
-    //Maze creation:
-    // for (let j = 0; j < mazeMap.length; j++) {
-    //     for (let i = 0; i < mazeMap[j].length; i++) {
-    //         if (mazeMap[i][j] == 0) { //determines whether a tile should be placed at position
-    //             fill(150);
-    //             stroke(100);
-    //             tilesArr[j * size + i] = new Tile(i * size, j * size, false);
-    //         } else {
-    //             fill(20, 20, 100);
-    //             noStroke();
-    //             tilesArr[j * size + i] = new Tile(i * size, j * size, true);
-    //         }
-    //         rect(tilesArr[j * size + i].getXTilePos(), tilesArr[j * size + i].getYTilePos(), size, size); //spawns tile
-    //     }
-    // }
-
-    //Canvas collision:
-    if (xUserPos < 0) {
-        xUserPos += speed; //causes collision with left canvas wall
-    }
-
-    if (xUserPos > (mazeMap.length * size) - size) {
-        xUserPos -= speed; //causes collision with right canvas wall
-    }
-
-    if (yUserPos < 0) {
-        yUserPos += speed; //causes collision with top canvas wall
-    }
-
-    if (yUserPos > (mazeMap.length * size) - size) {
-        yUserPos -= speed; //causes collision with bottom canvas wall
-    }
-
-    //Wall collision:
-    //xUserPos > tilesArr[i].getXTilePos() - size && xUserPos < tilesArr[i].getXTilePos() + size && yUserPos > tilesArr[i].getYTilePos() - size && yUserPos < tilesArr[i].getYTilePos() + size
-    
-    for (let i = 0; i < tilesArr.length; i++) {
-        if (xUserPos > tilesArr[i].getXTilePos() - size && xUserPos < tilesArr[i].getXTilePos() + size && yUserPos > tilesArr[i].getYTilePos() - size && yUserPos < tilesArr[i].getYTilePos() + size) {
-            console.log("contact");
-        }
-        if (keyIsPressed) {
-            console.log(xUserPos + " " + yUserPos + " " + tilesArr[i].getXTilePos() + " " + tilesArr[i].getYTilePos());
-            if (tilesArr[i].getIsWall() == true) {
-                console.log("wall has been hit... hopefully");
-                speed = speed * -1;
+class Grid { //a class that instatiates a grid object
+    tilesMap = new Map(); //creates a new empty map
+    constructor(mazeMap) {
+        for (let j = 0; j < mazeMap.length; j++) {
+            for (let i = 0; i < mazeMap[j].length; i++) { //loops around mazeMap 2D array
+                let gridRef = new GridRef(j * size, i * size); //creates grid reference object
+                let tile;
+                if (mazeMap[i][j] == 1) { //if value at position in 2D array is equal to a 1 then
+                    tile = new WallTile(gridRef, size); //creates new wall tile
+                } else {
+                    tile = new Tile(gridRef, size); //creates new tile
+                }
+                this.tilesMap.set(gridRef, tile); //adds new tile to map
             }
         }
     }
 
-    //Character movement:s
-    image(sprite, xUserPos, yUserPos, size, size); //spawns sprite
+    draw() {
+        for (let [gridRef, tile] of this.tilesMap) { //for every tile in map
+            tile.draw();
+        }
+    }
+}
 
-    if (keyIsDown(65)) {
-        xUserPos -= speed; //moves character left when A is being held
-    }
+function setup() { //function executes once on startup
+    createCanvas(mazeMap.length*size, mazeMap.length*size); //sets canvas size to array size
+    grid = new Grid(mazeMap); //creates new grid object
+}
+
+// function preload() {
+//     sprite = loadImage('http://127.0.0.1:5500/assets/sprite.png'); //preloads character image
+// }
+  
+function draw() { //function executes every tick (ms)
+    grid.draw(); //calls draw method in class Grid
+
+    //console.log(tile.getGridRef());
+
+//     //Canvas collision:
+//     if (xUserPos < 0) {
+//         xUserPos += speed; //causes collision with left canvas wall
+//     }
+
+//     if (xUserPos > (mazeMap.length * size) - size) {
+//         xUserPos -= speed; //causes collision with right canvas wall
+//     }
+
+//     if (yUserPos < 0) {
+//         yUserPos += speed; //causes collision with top canvas wall
+//     }
+
+//     if (yUserPos > (mazeMap.length * size) - size) {
+//         yUserPos -= speed; //causes collision with bottom canvas wall
+//     }
+
+//     //Wall collision:
+
+//     //Character movement:
+//     image(sprite, xUserPos, yUserPos, size, size); //spawns sprite
+
+//     if (keyIsDown(65)) {
+//         xUserPos -= speed; //moves character left when A is being held
+//     }
     
-    if (keyIsDown(68)) {
-        xUserPos += speed; //moves character right when D is being held
-    }
+//     if (keyIsDown(68)) {
+//         xUserPos += speed; //moves character right when D is being held
+//     }
     
-    if (keyIsDown(87)) {
-        yUserPos -= speed; //moves character up when W is being held
-    } 
+//     if (keyIsDown(87)) {
+//         yUserPos -= speed; //moves character up when W is being held
+//     } 
     
-    if (keyIsDown(83)) {
-        yUserPos += speed; //moves character down when S is being held
-    }
+//     if (keyIsDown(83)) {
+//         yUserPos += speed; //moves character down when S is being held
+//     }
 }
