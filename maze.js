43153935aab4ae13370e632c1a8d39d5f212885e
SamuelHill33 <06088@farnborough.ac.uk>
2021-07-08 11:52:36 +0100

    just before file split class transfer

--- maze.js
+++ maze.js
@@ -1,7 +1,6 @@
-let size = 30;
-let xUserPos = 300 - (size / 2);
-let yUserPos = 400 - (size / 2);
-let sprite;
+let size = 20;
+let xUserPos = 300;
+let yUserPos = 400;
 let speed = 2;
 
 let mazeMap = [ //2D array of tile positions in maze -- 0: path tile -- 1: wall tile -- 2: start tile -- 3: target tile -- 4: door tile -- 5: key tile -- 6: vent tile, etc...
@@ -39,17 +38,17 @@ let mazeMap = [ //2D array of tile positions in maze -- 0: path tile -- 1: wall 
 [1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]]
 
 class GridRef { //a class that instantiates a coordinate pair object
-    constructor(xTilePos, yTilePos) {
-        this.xTilePos = xTilePos;
-        this.yTilePos = yTilePos;
+    constructor(xTileRef, yTileRef) {
+        this.xTileRef = xTileRef;
+        this.yTileRef = yTileRef;
     }
 
-    getXTilePos() {
-        return this.xTilePos; //returns x position of coordinate pair
+    getXTileRef() {
+        return this.xTileRef; //returns x position of coordinate pair
     }
 
-    getYTilePos() {
-        return this.yTilePos; //returns y position of coordinate pair
+    getYTileRef() {
+        return this.yTileRef; //returns y position of coordinate pair
     }
 }
 
@@ -70,7 +69,7 @@ class Tile { //a class that instantiates a tile object
     draw() { //a method for displaying the tile object onto the canvas
         fill(200); //colours tile
         stroke(180); //outlines tile
-        rect(this.gridRef.getXTilePos(), this.gridRef.getYTilePos(), this.size, this.size); //spawns tile
+        rect(this.gridRef.getXTileRef(), this.gridRef.getYTileRef(), this.size, this.size); //spawns tile
     }
 }
 
@@ -82,7 +81,7 @@ class WallTile extends Tile { //a class that extends from a tile class and insta
     draw() { //wall tile has different draw method that will overwrite parent class draw method
         fill(100, 70, 50); 
         noStroke(); //removes outline (no outline)
-        rect(this.gridRef.getXTilePos(), this.gridRef.getYTilePos(), this.size, this.size); 
+        rect(this.gridRef.getXTileRef(), this.gridRef.getYTileRef(), this.size, this.size); 
     }
 }
 
@@ -110,55 +109,104 @@ class Grid { //a class that instatiates a grid object
     }
 }
 
+class Sprite {
+    constructor(image, gridRef, speed, size) {
+        this.image = image;
+        this.gridRef = gridRef;
+        this.speed = speed;
+        this.size = size;
+    }
+
+    getImage() {
+        return this.image;
+    }
+
+    setImage(newImage) {
+        this.image = newImage;
+    }
+
+    getGridRef() {
+        return this.gridRef;
+    }
+
+    getSpeed() {
+        return this.speed;
+    }
+
+    getSize() {
+        return this.size;
+    }
+}
+
+class UserSprite extends Sprite {
+    constructor(image, gridRef, speed, size) {
+        super(image, speed, size);
+        playerLoc = tile.getGridRef();
+        this.gridRef = playerLoc;
+    }
+
+    updateLoc() {
+
+    }
+
+    move() {
+    image(sprite.getImage(), xUserPos, yUserPos, size, size); //spawns sprite
+
+    if (keyIsDown(65)) {
+        xUserPos -= speed; //moves character left when A is being held
+    }
+    
+    if (keyIsDown(68)) {
+        xUserPos += speed; //moves character right when D is being held
+    }
+    
+    if (keyIsDown(87)) {
+        yUserPos -= speed; //moves character up when W is being held
+    } 
+    
+    if (keyIsDown(83)) {
+        yUserPos += speed; //moves character down when S is being held
+    }
+    }
+}
+
+class BotSprite extends Sprite {
+    constructor(image, gridRef, speed, size) {
+        super(image, gridRef, speed, size);
+    }
+}
+
 function setup() { //function executes once on startup
     createCanvas(mazeMap.length*size, mazeMap.length*size); //sets canvas size to array size
     grid = new Grid(mazeMap); //creates new grid object
+    //player = new UserSprite('http://127.0.0.1:5500/assets/sprite.png', playerStart, speed, size); //creates player sprite
+    console.log(testVar);
 }
 
 // function preload() {
-//     sprite = loadImage('http://127.0.0.1:5500/assets/sprite.png'); //preloads character image
+//     setImage('http://127.0.0.1:5500/assets/sprite.png'); //preloads character image
 // }
   
 function draw() { //function executes every tick (ms)
     grid.draw(); //calls draw method in class Grid
+    //player.move(); //calls the move method in class UserSprite
+}
 
-    //console.log(tile.getGridRef());
+function canvasCollision() {
+    //Canvas collision:
+    if (xUserPos < 0) {
+        xUserPos += speed; //causes collision with left canvas wall
+    }
 
-//     //Canvas collision:
-//     if (xUserPos < 0) {
-//         xUserPos += speed; //causes collision with left canvas wall
-//     }
+    if (xUserPos > (mazeMap.length * size) - size) {
+        xUserPos -= speed; //causes collision with right canvas wall
+    }
 
-//     if (xUserPos > (mazeMap.length * size) - size) {
-//         xUserPos -= speed; //causes collision with right canvas wall
-//     }
+    if (yUserPos < 0) {
+        yUserPos += speed; //causes collision with top canvas wall
+    }
 
-//     if (yUserPos < 0) {
-//         yUserPos += speed; //causes collision with top canvas wall
-//     }
-
-//     if (yUserPos > (mazeMap.length * size) - size) {
-//         yUserPos -= speed; //causes collision with bottom canvas wall
-//     }
-
-//     //Wall collision:
-
-//     //Character movement:
-//     image(sprite, xUserPos, yUserPos, size, size); //spawns sprite
-
-//     if (keyIsDown(65)) {
-//         xUserPos -= speed; //moves character left when A is being held
-//     }
-    
-//     if (keyIsDown(68)) {
-//         xUserPos += speed; //moves character right when D is being held
-//     }
-    
-//     if (keyIsDown(87)) {
-//         yUserPos -= speed; //moves character up when W is being held
-//     } 
-    
-//     if (keyIsDown(83)) {
-//         yUserPos += speed; //moves character down when S is being held
-//     }
+    if (yUserPos > (mazeMap.length * size) - size) {
+        yUserPos -= speed; //causes collision with bottom canvas wall
+    }
 }
